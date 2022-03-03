2a70afabff03b6e736e016709d9591a21bf23b71
Harshit Sharma <thisisharshitsharma0@gmail.com>
2022-03-03 22:24:51 +0530

    python notes

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,15 +1,31 @@
-#include<stdio.h>  
- int main()    
-{    
-int n, reverse=0, rem;    
-printf("Enter a number: ");    
-  scanf("%d", &n);    
-  while(n!=0)    
-  {    
-     rem=n%10;    
-     reverse=reverse*10+rem;    
-     n/=10;    
-  }    
-  printf("Reversed Number: %d",reverse);    
-return 0;  
-}   
+#include <stdio.h>
+int main(){
+    //a student marks in 5 subjects and calculate the average and using if else ladder give grades
+    int marks[5],i,total=0,a;
+    for(i=0;i<5;i++){
+        printf("Enter the marks of subject %d: ",i+1);
+        scanf("%d",&marks[i]);
+        total = total + marks[i];
+    }
+    a = total/5;
+    if(a<50 && a>=0){
+        printf("Fail");    }
+    else if(a>=50 && a<60) printf("Grade C");
+    else if(a>=60 && a<70){
+        printf("Grade B");
+    }
+    else if(a>=70 && a<80){
+        printf("Grade B+");
+    }
+    else if(a>=80 && a<90){
+        printf("Grade A");
+    }
+    else if(a>=90 && a<=100){
+        printf("Grade A+");
+    }
+    else{
+        printf("Invalid");
+    }
+    return 0;
+
+}
